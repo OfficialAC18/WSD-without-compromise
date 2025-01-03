8c2c75efd5bbf9088b37acb4d6842a983150e750
OfficialAC18 <akchunyachanchal@gmail.com>
2025-01-03 13:15:41 +0400

    Wrote DCI score andfixes to dsprites dataset class

---
+++ metrics/dci.py
@@ -0,0 +1,86 @@
+import scipy
+import torch
+from sklearn.ensemble import GradientBoostingClassifier, AdaBoostClassifier
+
+def compute_dci(ground_truth_data,
+                encoder_function,
+                num_train,
+                num_test,
+                surrogate_model = 'gbt',
+                bath_size = 32):
+    """
+    Compute disentanglement, completeness and informativeness scores (DCI).
+    Args:
+        random_state: int, random seed
+        ground_truth_data: DisentangledDataset, ground truth data
+        encoder_function: torch.nn.Module, encoder model which takes in input
+        and returns latent encodings of the input
+        num_train: int, number of training samples
+        num_test: int, number of testing samples
+        surrogate_model: str, surrogate model to use
+        bath_size: int, batch size for the dataloader
+    
+    Returns:
+        scores: dict, average disentanglement, completeness and informativeness scores
+        for the train and test sets.
+    """
+    #Generate the train set
+    inputs_train, y_train = ground_truth_data.sample_observations(num_train,
+                                                             return_factors=True)
+    x_train = encoder_function(inputs_train)
+
+    #Generate the test set
+    inputs_test, y_test = ground_truth_data.sample_observations(num_test,
+                                                           return_factors=True)
+    x_test = encoder_function(inputs_test)
+
+    assert x_train.shape[0] == num_train
+    assert y_train.shape[0] == num_train
+    assert x_test.shape[0] == num_test
+    assert y_test.shape[0] == num_test
+
+    #Compute Informativeness
+    y_shape = y_train.shape[1]
+    x_shape = x_train.shape[1]
+    importance_matrix = torch.zeros(x_shape, y_shape,
+                                    dtype=torch.float64)
+    
+    train_loss = []
+    test_loss = []
+    for i in range(y_shape):
+        if surrogate_model == 'gbt':
+            model = GradientBoostingClassifier()
+        elif surrogate_model == 'adaboost':
+            model = AdaBoostClassifier(n_estimators=100)
+        
+        model.fit(x_train.detach().numpy(), y_train[:,i].detach().numpy())
+        importance_matrix[:,i] = torch.tensor(model.feature_importances_)
+        train_loss.append(torch.mean(model.predict(x_train.detach().numpy()) == y_train[:,i].detach().numpy()))
+        test_loss.append(torch.mean(model.predict(x_test.detach().numpy()) == y_test[:,i].detach().numpy()))
+
+    train_loss = torch.mean(train_loss)
+    test_loss = torch.mean(test_loss)
+
+    #Compute Disentanglement
+    per_latent_entropy = 1 - scipy.stats.entropy(importance_matrix.detach().numpy().T + 1e-12,
+                                                 base = y_shape)
+    
+    if torch.sum(importance_matrix) == 0:
+        importance_matrix = torch.ones_like(importance_matrix)
+    
+    latent_importance = torch.sum(importance_matrix, dim=1)/torch.sum(importance_matrix)
+    disentanglement = torch.sum(latent_importance*per_latent_entropy)
+
+    #Compute Completeness
+    per_factor_entropy = 1 - scipy.stats.entropy(importance_matrix.detach().numpy() + 1e-12,
+                                                 base = x_shape) 
+    
+    factor_importance = torch.sum(importance_matrix, dim=0)/torch.sum(importance_matrix)
+    completeness = torch.sum(factor_importance*per_factor_entropy)
+
+    scores = {}
+    scores['informative_train'] = train_loss
+    scores['informative_test'] = test_loss
+    scores['disentanglement'] = disentanglement
+    scores['completeness'] = completeness
+    return scores
